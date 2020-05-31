1bd48e77cfb6d10ac60ca42e58518153a3a2ffa8
Noah Oller Smith <nosolls@protonmail.com>
2020-05-31 15:48:49 -0400

    Trying default keybindings

--- init.el
+++ init.el
@@ -1,3 +1,37 @@
+;; Garbage collection 
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+	gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
+;; melpa
+
+(require 'package)
+(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org"   . "https://orgmode.org/elpa/")))
+(package-initialize)
+;; use package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+;; link config file
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+;; custom stuff
+
 ;; Garbage collection
 (setq gc-cons-threshold 402653184
       gc-cons-percentage 0.6)
@@ -35,10 +69,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (leuven)))
+ '(custom-safe-themes
+   (quote
+    ("8b8bd4e8723819b49f79bbeae2479e6661206bfc1f9953c9ed3a58c31ff51e92" default)))
  '(package-selected-packages
    (quote
-    (erc-hl-nicks async docker-compose-mode dockerfile-mode diminish company rainbow-mode rainbow-delimiters smex ido-vertical-mode org-bullets dashboard sudo-edit hungry-delete beacon emojify aggressive-indent which-key avy switch-window xah-fly-keys use-package))))
+    (spaceline erc-hl-nicks async docker-compose-mode dockerfile-mode diminish company rainbow-mode rainbow-delimiters smex ido-vertical-mode org-bullets dashboard sudo-edit hungry-delete beacon emojify aggressive-indent which-key use-package switch-window avy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
