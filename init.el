f169c9f0ea5b52e52343dd8cf3f34baca531b7b1
Noah Oller Smith <nosolls@protonmail.com>
2020-10-22 11:53:16 -0400

    Additions

--- init.el
+++ init.el
@@ -31,18 +31,3 @@
 ;; Link config file
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(counsel avy switch-window xah-fly-keys magit erc-hl-nicks async docker-compose-mode dockerfile-mode diminish company rainbow-delimiters org-bullets sudo-edit emojify aggressive-indent which-key moe-theme use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#fdfde7" :foreground "#5f5f5f" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width condensed :foundry "xos4" :family "Terminus")))))
-
